1705b590fdf65d629005793323a32189f5a363bd
Fashola Ayodeji <fashtop3@gmail.com>
2017-04-20 20:32:29 +0100

    sending data concurrently

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -3,11 +3,8 @@
 
 #define SSID        "ADYbWU";//"SUCCESS"                                        // Wifi SSID
 #define PASS        "nokia66542";//"olatundeayo44"                                    // WiFi Password
-#define DEST_HOST   "api.wunderground"                            // API site host
-#define DEST_IP     "192.254.235.21"                              // API site IP address
-#define TIMEOUT     5000                                          // mS
-#define CONTINUE    false                                         // Define for readability
-#define HALT        true                                          // Define for readibility
+                         // mS
+#define STATION_ID  1
 
 
 boolean echoCommand(String, const char *, boolean, int);
@@ -15,9 +12,12 @@ void echoSkip();
 boolean expect(const char *, uint16_t);
 boolean connectWiFi();
 void errorHalt();
+void connectToHost(String, int);
 
 int buf_size = 64;
-volatile char serialbuffer[1000];//serial buffer for request url
+const String HOST = "192.168.43.103";
+const int PORT = 8000;
+
 SoftwareSerial ESP8266(10,11); // RX (goes to TX on ESP), TX (goes to RX on ESP)
 
 
@@ -76,122 +76,104 @@ void setup()
     
   if (connection_established)   {           // If connection attempts failed
 
-    String cmd = "+CIPSTART=\"TCP\",\""; cmd += "erp.rockcityfmradio.com"; cmd += "\",80"; // Establish TCP connection
-    _delay_ms(2000);
-    Serial.println("Connecting to Host...!!!");
-    Serial.println(cmd);
-
-    while(!expect_AT_OK("+CIPSTART=\"TCP\",\"erp.rockcityfmradio.com\",80", 6000)) {
-//      expect_scan(F("+SAPBR: 1,%hu,\"%*hu.%*hu.%*hu.%*hu\""), &status, 3000); //+SAPBR: 1,1,"10.96.42.184"
-      //Todo: limit loop
-      _delay_ms(4000);
-      Serial.println("Connecting to Host...!!!");
-      expect_AT_OK("+CIPCLOSE", 3000);
-//      if(expect("ALREAY CONNECT", 6000)) break;
-      eat_echo();
-    }
-
-    Serial.println("Connected to Host...!!!");
-    delay(2000);                            // Indicate there was an error
+      reConnectToHost(HOST, PORT);
+      delay(2000);                            // Indicate there was an error
   }
 
 }
 
 void loop()
 {
-  //output everything from ESP8266 to the Arduino Micro Serial output
+
 //  while (ESP8266.available() > 0) {
-//    Serial.write(ESP8266.read());
-//  }
-//  
-//  if (Serial.available() > 0) {
-//     //read from serial until terminating character
-//     int len = Serial.readBytesUntil('\n', serialbuffer, sizeof(serialbuffer));
-//  
-//     //trim buffer to length of the actual message
-//     String message = String(serialbuffer).substring(0,len-1);
-//     Serial.println("message: " + message);
-// 
-//     //check to see if the incoming serial message is a url or an AT command
-//     if(message.substring(0,2)=="AT"){
-//       //make command request
-//       Serial.println("COMMAND REQUEST");
-//       ESP8266.println(message); 
-//     }else{
-//      //make webrequest
-//       Serial.println("WEB REQUEST");
-//       WebRequest(message);
-//     }
-//  }
+//      Serial.write(ESP8266.read());
+//    }
+
+    eat_echo();
+    
+    WebRequest(45, 23, 22);
+    delay(3000);
+
+    reConnectToHost(HOST, PORT);
 }
 
-////web request needs to be sent without the http for now, https still needs some working
-//void WebRequest(String request){
-// //find the dividing marker between domain and path
-//     int slash = request.indexOf('/');
-//     
-//     //grab the domain
-//     String domain;
-//     if(slash>0){  
-//       domain = request.substring(0,slash);
-//     }else{
-//       domain = request;
-//     }
-//
-//     //get the path
-//     String path;
-//     if(slash>0){  
-//       path = request.substring(slash);   
-//     }else{
-//       path = "/";          
-//     }
-//     
-//     //output domain and path to verify
-//     Serial.println("domain: |" + domain + "|");
-//     Serial.println("path: |" + path + "|");     
-//     
-//     //create start command
-//     String startcommand = "+CIPSTART=\"TCP\",\"" + domain + "\", 80"; //443 is HTTPS, still to do
-//     
-//     ESP8266.println(startcommand);
-//     Serial.println(startcommand);
-//     
-//     
-//     //test for a start error
-//     if(ESP8266.find("Error")){
-//       Serial.println("error on start");
-//       return;
-//     }
-//     
-//     //create the request command
-//     String sendcommand = "GET http://"+ domain + path + " HTTP/1.0\r\n\r\n\r\n";//works for most cases
-//     
-//     Serial.print(sendcommand);
-//     
-//     //send 
-//     ESP8266.print("+CIPSEND=");
-//     ESP8266.println(sendcommand.length());
-//     
-//     //debug the command
-//     Serial.print("+CIPSEND=");
-//     Serial.println(sendcommand.length());
-//     
-//     //delay(5000);
-//     if(ESP8266.find(">"))
-//     {
-//       Serial.println(">");
-//     }else
-//     {
-//       ESP8266.println("+CIPCLOSE");
-//       Serial.println("connect timeout");
-//       delay(1000);
-//       return;
-//     }
-//     
-//     //Serial.print(getcommand);
-//     ESP8266.print(sendcommand); 
-//}
+//web request needs to be sent without the http for now, https still needs some working
+void WebRequest(double petro, double kero, double diesel){
+     //test for a start error
+     
+      
+     //create the request command
+     String sendcommand = "GET /api/station/"; sendcommand += STATION_ID;   
+     sendcommand += "/data/"; 
+     sendcommand += petro; 
+     sendcommand += "/";
+     sendcommand += kero; 
+     sendcommand +="/"; 
+     sendcommand += diesel; 
+     sendcommand += " HTTP/1.0\r\n\r\n";
+     
+     //send 
+     ESP8266.print("AT+CIPSEND=");
+     ESP8266.println(sendcommand.length());
+     
+     //debug the command
+     Serial.print("AT+CIPSEND=");
+     Serial.println(sendcommand.length());
 
+    
+    
+     if(ESP8266.find(">"))
+     {
+       Serial.print("> ");
+       ESP8266.print(sendcommand);
+       Serial.print(sendcommand);
+       _delay_ms(3000);
+        eat_echo();
+     }
+}
+
+
+void connectToHost(String host, int port)
+{
+
+      expect_AT_OK("+CIPCLOSE", 3000);
+      _delay_ms(3000);
+      String cmd = "+CIPSTART=\"TCP\",\""; cmd += host; cmd += "\","; cmd += port; // Establish TCP connection
+      char host_buf[100]; 
+      cmd.toCharArray(host_buf, 100);
+      _delay_ms(2000);
+      Serial.println("Connecting to Host...!!!");
+      Serial.println(host_buf);
+  
+      while(!expect_AT_OK(host_buf/*"+CIPSTART=\"TCP\",\"192.168.43.94\",80"*/, 6000)) {
+        _delay_ms(5000);
+        Serial.println("Connecting to Host...!!!");
+        expect_AT_OK("+CIPCLOSE", 3000);
+        eat_echo();
+    }
+
+    Serial.println("Connected to Host...!!!");
+}
+
+void reConnectToHost(const String host, const int port)
+{
+      expect_AT_OK("+CIPCLOSE", 3000);
+      _delay_ms(1000);
+      String cmd = "+CIPSTART=\"TCP\",\""; cmd += host; cmd += "\","; cmd += port; // Establish TCP connection
+      char host_buf[100]; 
+      cmd.toCharArray(host_buf, 100);
+      Serial.println("Connecting to Host...!!!");
+      Serial.println(host_buf);
+  
+      while(!expect_AT_OK(host_buf/*"+CIPSTART=\"TCP\",\"192.168.43.94\",80"*/, 6000)) {
+        _delay_ms(5000);
+        Serial.println("Connecting to Host...!!!");
+        expect_AT_OK("+CIPCLOSE", 3000);
+        eat_echo();
+    }
+
+    Serial.println("Connected to Host...!!!");
+}
 
 bool expect_AT(const char *cmd, const char *expected, uint16_t timeout)
 {
@@ -277,8 +259,11 @@ boolean connectWiFi()
   
 //  Serial.println(cmd_buf);
   
-  if (expect_AT_OK(cmd_buf, 8000))                                               // Join Access Point
+  if (expect_AT_OK(cmd_buf, 8000))  {
+    // Join Access Point
+    eat_echo();
     return true;
+  }
   else
     return false;
 }
--- water/water.ino
+++ water/water.ino
@@ -7,12 +7,26 @@
 
 #include <util/delay.h>
 
-#define PETRO_TRIG  5
-#define PETRO_ECHO  6
-#define TANK_HEIGHT 400   //in cm
+#define DIESEL_TRIG  2
+#define DIESEL_ECHO  3
+
+#define PETRO_TRIG   4
+#define PETRO_ECHO   5
+
+#define KEROS_TRIG   6
+#define KEROS_ECHO   7
+
+#define CONNECT_WIFI_LED 12
+#define CONNECT_HOST_LED 13
+
+// It is possible we dont use equal tank
+#define DIESEL_TANK_HEIGHT  400.00   //in cm
+#define KEROS_TANK_HEIGHT   300.00   //in cm
+#define PETROL_TANK_HEIGHT  350.00   //in cm
   
 
-volatile double petrolDuration, petrolDistance, petrolLevel;
+volatile double duration, distance;
+volatile double keroseneLevel, dieselLevel,  petrolLevel;
   
 
 void setup() {
@@ -22,31 +36,82 @@ void setup() {
   delay(1500);
   
   // Note: verify the following code 
-  pinMode(PETRO_TRIG, OUTPUT);     // Digital PIN 5
-  pinMode(PETRO_ECHO, INPUT);      // Digital PIN 6
+
+  pinMode(DIESEL_TRIG, OUTPUT);    // Digital PIN 2
+  pinMode(DIESEL_ECHO, INPUT);     // Digital PIN 3
+  
+  pinMode(PETRO_TRIG, OUTPUT);     // Digital PIN 4
+  pinMode(PETRO_ECHO, INPUT);      // Digital PIN 5
+  
+  pinMode(KEROS_TRIG, OUTPUT);     // Digital PIN 6
+  pinMode(KEROS_ECHO, INPUT);      // Digital PIN 7
+  
+
+  pinMode(CONNECT_WIFI_LED, OUTPUT);  // Digital PIN 12
+  pinMode(CONNECT_HOST_LED, OUTPUT);  // Digital PIN 13
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  /**
+   * move to interrupt
+   */
+//  // if connected to wifi network 
+//  blink_signal(CONNECT_WIFI_LED);
+//
+//  // if connected to host network
+//  blink_signal(CONNECT_HOST_LED);
   
-  /// The following code cater for measuring petrol
-  petrolLevel = distance_detect(PETRO_TRIG, PETRO_ECHO);
   
-  Serial.print("The Level of the Water tank is: ");
-  Serial.println(petrolLevel);
-  _delay_ms(1000);
+  // The following code is working to accept the level
+  // of the Three materials
+  dieselLevel = distance_detect(DIESEL_TRIG, DIESEL_ECHO, DIESEL_TANK_HEIGHT);
+  keroseneLevel = distance_detect(KEROS_TRIG, KEROS_ECHO, KEROS_TANK_HEIGHT);
+  petrolLevel = distance_detect(PETRO_TRIG, PETRO_ECHO, PETROL_TANK_HEIGHT);
+
+  /**
+   * Send data to web
+   */
+
+
+  // The below section is to print onto  the serial Monitor
+  Serial.print("The Level of the Diesel tank is: ");
+  print_level(dieselLevel);
+
+  Serial.print("The Level of the Kerosene tank is: ");
+  print_level(keroseneLevel);
+
+  Serial.print("The Level of the Petrol tank is: ");
+  print_level(petrolLevel);
 
 }
 
-double distance_detect(uint8_t trig_pin, uint8_t echo_pin)
+double distance_detect(uint8_t trig_pin, uint8_t echo_pin, double tank_height)
 {
   digitalWrite(trig_pin, LOW);
   delayMicroseconds(2);
   digitalWrite(trig_pin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig_pin, LOW);
-  petrolDuration = pulseIn(echo_pin, HIGH);
-  petrolDistance = static_cast<double>(petrolDuration)/58.2;
-  return (TANK_HEIGHT - petrolDistance);
+  duration = pulseIn(echo_pin, HIGH);
+  distance = static_cast<double>(duration)/58.2;
+  return (tank_height - distance);
 }
+
+void print_level(double level_value)
+{
+  Serial.println(level_value);
+  _delay_ms(1000);
+}
+
+void blink_signal(uint8_t signal_pin )
+{
+  digitalWrite( signal_pin, HIGH);
+  _delay_ms(1000);
+  digitalWrite( signal_pin, LOW);
+  _delay_ms(1000);
+}
+
+
+  
 
