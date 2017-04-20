27ba9fcbfae5dbf18f425a4c75de85e0c842e320
Fashola Ayodeji <fashtop3@gmail.com>
2017-04-20 20:48:20 +0100

    added ultra sonic code

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -4,9 +4,40 @@
 #define SSID        "ADYbWU";//"SUCCESS"                                        // Wifi SSID
 #define PASS        "nokia66542";//"olatundeayo44"                                    // WiFi Password
                          // mS
-#define STATION_ID  1
+#define STATION_ID  1 // station ID
+
+#define DIESEL_TRIG  2
+#define DIESEL_ECHO  3
+
+#define PETRO_TRIG   4
+#define PETRO_ECHO   5
+
+#define KEROS_TRIG   6
+#define KEROS_ECHO   7
+
+#define CONNECT_WIFI_LED 12
+#define CONNECT_HOST_LED 13
+
+// It is possible we dont use equal tank
+#define DIESEL_TANK_HEIGHT  400.00   //in cm
+#define KEROS_TANK_HEIGHT   300.00   //in cm
+#define PETROL_TANK_HEIGHT  350.00   //in cm
+  
+
+/**
+ * Global variable declearation
+ */
+volatile double duration, distance;
+volatile double keroseneLevel, dieselLevel,  petrolLevel;
+
+int buf_size = 64;
+const String HOST = "192.168.43.103";
+const int PORT = 8000;
 
 
+/**
+ * function protypes
+ */
 boolean echoCommand(String, const char *, boolean, int);
 void echoSkip();
 boolean expect(const char *, uint16_t);
@@ -14,22 +45,55 @@ boolean connectWiFi();
 void errorHalt();
 void connectToHost(String, int);
 
-int buf_size = 64;
-const String HOST = "192.168.43.103";
-const int PORT = 8000;
-
 SoftwareSerial ESP8266(10,11); // RX (goes to TX on ESP), TX (goes to RX on ESP)
 
 
 void setup()
 {
+
+  ultra_module_config();
+  wifi_setup();
+}
+
+void loop()
+{
+
+//  while (ESP8266.available() > 0) {
+//      Serial.write(ESP8266.read());
+//    }
+
+    eat_echo();
+
+    // The following code is working to accept the level
+    // of the Three materials
+    dieselLevel = distance_detect(DIESEL_TRIG, DIESEL_ECHO, DIESEL_TANK_HEIGHT);
+    keroseneLevel = distance_detect(KEROS_TRIG, KEROS_ECHO, KEROS_TANK_HEIGHT);
+    petrolLevel = distance_detect(PETRO_TRIG, PETRO_ECHO, PETROL_TANK_HEIGHT);
+
+    // The below section is to print onto  the serial Monitor
+    Serial.print("The Level of the Diesel tank is: ");
+    print_level(dieselLevel);
+  
+    Serial.print("The Level of the Kerosene tank is: ");
+    print_level(keroseneLevel);
+  
+    Serial.print("The Level of the Petrol tank is: ");
+    print_level(petrolLevel);
+    
+    WebRequest(petrolLevel, keroseneLevel, dieselLevel);
+    delay(3000);
+
+    reConnectToHost(HOST, PORT);
+}
+
+/**
+ * setting up wifi module connections
+ */
+void wifi_setup()
+{
   ESP8266.begin(9600); //connection to ESP8266
   Serial.begin(9600); //serial debug
-
-//  ESP8266.setTimeout(TIMEOUT);              // Set the timeout value for the ESP8266
   
-  //set mode needed for new boards
-
   _delay_ms(1000);
   Serial.println("Restarting...");
   if(expect_AT("+RST", "OK", 2000)) {
@@ -70,7 +134,6 @@ void setup()
       }
     }
 
-
     expect_AT_OK("+CIPCLOSE", 3000);
     _delay_ms(3000);
     
@@ -79,29 +142,45 @@ void setup()
       reConnectToHost(HOST, PORT);
       delay(2000);                            // Indicate there was an error
   }
-
 }
 
-void loop()
+void print_level(double level_value)
 {
+  Serial.println(level_value);
+  _delay_ms(1000);
+}
 
-//  while (ESP8266.available() > 0) {
-//      Serial.write(ESP8266.read());
-//    }
+void ultra_module_config()
+{
+  pinMode(DIESEL_TRIG, OUTPUT);    // Digital PIN 2
+  pinMode(DIESEL_ECHO, INPUT);     // Digital PIN 3
+  
+  pinMode(PETRO_TRIG, OUTPUT);     // Digital PIN 4
+  pinMode(PETRO_ECHO, INPUT);      // Digital PIN 5
+  
+  pinMode(KEROS_TRIG, OUTPUT);     // Digital PIN 6
+  pinMode(KEROS_ECHO, INPUT);      // Digital PIN 7
+  
 
-    eat_echo();
-    
-    WebRequest(45, 23, 22);
-    delay(3000);
+  pinMode(CONNECT_WIFI_LED, OUTPUT);  // Digital PIN 12
+  pinMode(CONNECT_HOST_LED, OUTPUT);  // Digital PIN 13
+}
 
-    reConnectToHost(HOST, PORT);
+double distance_detect(uint8_t trig_pin, uint8_t echo_pin, double tank_height)
+{
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  duration = pulseIn(echo_pin, HIGH);
+  distance = static_cast<double>(duration)/58.2;
+  return (tank_height - distance);
 }
 
 //web request needs to be sent without the http for now, https still needs some working
 void WebRequest(double petro, double kero, double diesel){
-     //test for a start error
-     
-      
+
      //create the request command
      String sendcommand = "GET /api/station/"; sendcommand += STATION_ID;   
      sendcommand += "/data/"; 
@@ -119,9 +198,7 @@ void WebRequest(double petro, double kero, double diesel){
      //debug the command
      Serial.print("AT+CIPSEND=");
      Serial.println(sendcommand.length());
-
-    
-    
+        
      if(ESP8266.find(">"))
      {
        Serial.print("> ");
